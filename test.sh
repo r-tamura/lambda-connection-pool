363183ff37e79c4493a159e59819e2c261e82d5b
r-tamura <rtamura30@gmail.com>
2021-08-13 12:33:25 +0900

    テストスクリプトの接続先エンドポイントを引数として受け取るように

--- test.sh
+++ test.sh
@@ -4,10 +4,22 @@ set -e
 INTERVAL_SEC=180
 REQUEST_COUNT=2
 
+usage () {
+  echo 'usage:'
+  echo " test.sh <endpoint>"
+}
+
+if [[ $1 == '' ]]
+then
+  usage
+  echo 'Error: endpoint is required to be set'
+  exit 1
+fi
+
 for i in $(seq $REQUEST_COUNT)
 do
   printf "$(date) "
-  curl https://aamhyi19dh.execute-api.us-east-1.amazonaws.com/prod/
+  curl "$1"
   printf '\n'
 
   if [[ $i -ne $REQUEST_COUNT ]]
